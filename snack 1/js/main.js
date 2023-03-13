c41010feacfcb7357ee5457a6639ec429fbe83db
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-03-13 21:24:43 +0100

    correzione primo snack

--- snack 1/js/main.js
+++ snack 1/js/main.js
@@ -15,16 +15,15 @@ const bici = [
     }
 ];
 
-const {nome, peso,} = bici;
-const risultato = [];
-
-
-bici.forEach(elemento => {
-    if (elemento.peso <= 7) {
-        risultato.push(`${elemento.nome} ${elemento.peso} è la più leggera`);
-    } else {
-        risultato.push(`${elemento.nome} ${elemento.peso} troppo pesante`);
+let pesoBici = 0;
+for (let i = 0 ; i < bici.length; i++) {
+    if (bici[i].peso < bici[pesoBici].peso){
+        pesoBici = i;
     }
-});
+}
 
-console.log(risultato);
+console.log(bici[pesoBici]);
+
+const {nome, peso} = bici[pesoBici];
+const biciDom = document.getElementById('bici');
+biciDom.innerHTML = `la bici più leggera è la ${nome} con il peso di ${peso} kg`; 
